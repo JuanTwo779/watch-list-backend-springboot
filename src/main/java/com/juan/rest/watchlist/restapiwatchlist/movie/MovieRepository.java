a0555a9af5c43080e664e1c81d1622fc3da6942a
juanm <juanmangubat@gmail.com>
2024-11-17 17:32:55 +1100

    Random movie method

--- src/main/java/com/juan/rest/watchlist/restapiwatchlist/movie/MovieRepository.java
+++ src/main/java/com/juan/rest/watchlist/restapiwatchlist/movie/MovieRepository.java
@@ -1,16 +1,22 @@
 package com.juan.rest.watchlist.restapiwatchlist.movie;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Random;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    Logger logger = LoggerFactory.getLogger(MovieRepository.class);
     List<Movie> findByUsername(String username);
+    List<Movie> findByUsernameAndWatchedFalse(String username);
 
     default Movie findRandomMovie(String username){
-        List<Movie> list = findByUsername(username);
+        List<Movie> list = findByUsernameAndWatchedFalse(username);
 
+        //Log the list of movies
+//        logger.debug("Unwatched movies for {}: {}", username, list);
         Random rng = new Random();
         int randomMovie = rng.nextInt(list.size());
 
